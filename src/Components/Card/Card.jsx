5fcebbf1b0a53d118875a683630bd90ee08eef23
Ira Bukhaeva <irina.bukhaeva@yandex.ru>
2024-07-18 13:39:08 +0800

    add slider

--- src/Components/Card/Card.jsx
+++ src/Components/Card/Card.jsx
@@ -1,23 +1,21 @@
-import React, { useState } from 'react'
-import words from "../../words.json"
-import style from "./Card.module.scss";
+import React, { useState, useEffect } from 'react';
+import style from './Card.module.scss';
 
+export default function Card({word}) {
+  const [flag, setFlag]=useState(false);
+  useEffect(() => {setFlag(false);},[word])
 
-export default function Card() {
-    const wordIndex = Math.floor(Math.random()*Object.keys(words).length);
-    const word = words[wordIndex].english;
-    const transcription = words[wordIndex].transcription;
-    const translation = words[wordIndex].russian;
-    const [translateClicked, setTranslateClicked] = useState(false);
-    const handleTranslateClicked = () => {
-        setTranslateClicked(!translateClicked)    
-      };
-    
-    return (
-        <div className={style.card}>
-        <div className={style.word}>{word}</div>
-        <div className={style.word}>{transcription}</div>
-        <div className={style.word}>{translateClicked ? <div onClick = {handleTranslateClicked}>{translation}</div> : <button onClick = {handleTranslateClicked} className={style.button}>Перевод</button>}</div>
-      </div>
-  )
+    return (          
+          <div className={style.card}>
+              <div className={style.word}>{word.english}</div>
+              <div className={style.word}>{word.transcription}</div>
+              {flag ? (
+                      <div onClick={()=>setFlag(false)} className={style.word}>{word.russian}</div>
+                  ) : (
+                      <button onClick={()=>setFlag(true)} className={style.button}>
+                          Перевод
+                      </button>
+                  )}              
+          </div>        
+    );
 }
